9956435442096a0f69a354bb4641bee924cf9d02
amelosbc <alexandremelo52@gmail.com>
2022-03-12 10:09:34 -0300

    Security config

--- dsmove/frontend/src/components/Navbar/index.tsx
+++ dsmove/frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <header>
         <nav className = "container"> 
           <div className = "dsmovie-nav-content">
-            <h1>DSMovie</h1>
+            <h1>ASMovies</h1>
             <a href="https://github.com/amelosbc">
               <div className = "dsmovie-contact-container">
                 <GithubIcon />
