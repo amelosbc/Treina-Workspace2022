66ae39a5e6c9503fbdede2a9e128a736cb3a84c9
amelosbc <alexandremelo52@gmail.com>
2022-03-15 00:36:40 -0300

    adicionado readme

--- dsmove/frontend/src/components/MovieScore/index.tsx
+++ dsmove/frontend/src/components/MovieScore/index.tsx
@@ -3,8 +3,8 @@ import './style.css';
 
 function MovieScore() {
 
-    const score = 3.5;
-    const count = 13;
+     const score = 3.5;
+     const count = 13;
     return (
         <div className="dsmovie-score-container" >
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
