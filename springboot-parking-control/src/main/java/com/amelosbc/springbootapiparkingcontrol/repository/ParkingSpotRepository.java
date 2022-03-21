71bc31e44a812dc05866f0ba053c10c55c6cfe4b
amelosbc <alexandremelo52@gmail.com>
2022-03-21 20:29:30 -0300

    Adicionado projeto ParkingControl

---
+++ springboot-parking-control/src/main/java/com/amelosbc/springbootapiparkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,23 @@
+package com.amelosbc.springbootapiparkingcontrol.repository;
+
+import java.awt.print.Pageable;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amelosbc.springbootapiparkingcontrol.model.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+
+	boolean existsByLicensePlateCar(String licensePlateCar);
+
+	boolean existsByParkingSpotNumber(String parkingSpotNumber);
+
+	boolean existsByApartmentAndBlock(String apartment, String block);
+
+	Page<ParkingSpotModel> findAll(Pageable pageable);
+
+}
