02ca4668f508bada3af2217a2b04973b37bb26d9
amelosbc <alexandremelo52@gmail.com>
2022-05-08 22:10:57 -0300

    Finalizado FrontEnd do projeto Bankline controle de movimentocoes

--- ProjetosDIO/santander-dev-week-bankline-app/src/app/app.component.ts
+++ ProjetosDIO/santander-dev-week-bankline-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'santander-dev-week-bankline-app';
+  title = 'Bankline - Controle de Movimentacoes';
 }
